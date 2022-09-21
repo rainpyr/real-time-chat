449e20cb7b4a03920593d20299d5fdce0b2285e8
Dee Pabst <dee@canva.com>
2022-09-21 12:21:35 +1000

    added stats controller actions

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -4,19 +4,29 @@ class StatsController < ApplicationController
   def create
     @stat = Stat.create(
       name: params[:name],
-      count: 0,
+      count: 0, # initial value
     )
+    render json: @stat
   end
 
   def index
+    render json: Stat.all
   end
 
   def show
+    @stat = Stat.find params[:id]
+    render json: @stat
   end
 
   def update
+    stat = Stat.find params[:id]
+    stat.update(
+      name: params[:name],
+      count: params[:count],
+    )
   end
 
   def destroy
+    Stat.destroy params[:id]
   end
 end
