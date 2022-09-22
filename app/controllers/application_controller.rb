200ce0a7b58c8fe088b6e442d4d1a86f9e4e9817
Dee Pabst <dee@canva.com>
2022-09-22 14:35:14 +1000

    deployment woes - adding raise false to the skip authenticity check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # To turn off CSRF completely, disable it in ApplicationController:
   # skip_forgery_protection
-  skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token, raise: false
 
   include Knock::Authenticable
 end
