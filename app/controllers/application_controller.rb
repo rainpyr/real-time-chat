64eab5d75d34bd3441ca72500eb8f2df8b79a21d
Dee Pabst <dee@canva.com>
2022-09-22 13:25:25 +1000

    turn off forgery protection yolo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
-  skip_before_action :verify_authenticity_token
+  # To turn off CSRF completely, disable it in ApplicationController:
+  skip_forgery_protection
+  # skip_before_action :verify_authenticity_token
 
   include Knock::Authenticable
 end
