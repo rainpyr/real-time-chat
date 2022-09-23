fec6a447518c389a3dd3ac86560ec88dea4a8f18
Dee Pabst <dee@canva.com>
2022-09-23 13:56:09 +1000

    HACK - secret in knock file

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,27 +1,30 @@
-module ApplicationCable        
-  class Connection < ActionCable::Connection::Base 
+module ApplicationCable
+  class Connection < ActionCable::Connection::Base
     identified_by :current_user
- 
-    def connect                
+
+    def connect
       self.current_user = find_verified_user
     end
- 
+
     private
-      def find_verified_user   
-        # begin
-          token = request.headers[:HTTP_SEC_WEBSOCKET_PROTOCOL].split(' ').last
-          p 'token', token
-          decoded_token = JWT.decode token, Rails.application.secrets.secret_key_base, true, { :algorithm => 'HS256' }
-          p 'decoded-token', decoded_token
-          current_user = User.find((decoded_token[0])['sub'])
-          if current_user.present?
-            current_user
-          else                 
-            reject_unauthorized_connection
-          end
-        # rescue
-        #   reject_unauthorized_connection  
-        # end
+
+    def find_verified_user
+      # begin
+      token = request.headers[:HTTP_SEC_WEBSOCKET_PROTOCOL].split(" ").last
+      p "token", token
+      # p "Rails.application.secrets.secret_key_base", Rails.application.secrets.secret_key_base
+      # decoded_token = JWT.decode token, Rails.application.secrets.secret_key_base, true, { :algorithm => "HS256" }
+      decoded_token = JWT.decode token, "secret", true, { :algorithm => "HS256" }
+      p "decoded-token", decoded_token
+      current_user = User.find((decoded_token[0])["sub"])
+      if current_user.present?
+        current_user
+      else
+        reject_unauthorized_connection
       end
+      # rescue
+      #   reject_unauthorized_connection
+      # end
+    end
   end
 end
